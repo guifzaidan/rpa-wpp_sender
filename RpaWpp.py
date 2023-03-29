4b8655635690b8d283b7bbd6bd2ff1baf3dc4149
Guilherme Zaidan <gfzaidan57@gmail.com>
2023-03-29 12:48:58 -0300

    WppSender class finished

--- RpaWpp.py
+++ RpaWpp.py
@@ -4,16 +4,24 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
-from datetime import datetime
-# import urllib
+import urllib
 import time
 
 
 class WppSender():
 
+    # Global Vars
+    footer_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p'
+    send_button_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
+
     @staticmethod
-    def _sleep_time():
+    def _cycle_sleep_time():
         time.sleep(30)
+
+    @staticmethod
+    def _message_format(message:str):
+        message_mod = urllib.parse.quote(f"{message}")
+        return message_mod
 
     def __init__(self, headless:bool):
         self._service = Service(ChromeDriverManager().install())
@@ -31,9 +39,9 @@ class WppSender():
 
     def get_wpp_site(self):
         self._driver.get("https://web.whatsapp.com/")
-        self._wait_loading()
+        self._initial_wait()
 
-    def _wait_loading(self):
+    def _initial_wait(self):
         while len(self._driver.find_elements(By.ID, "side")) < 1:
             self._get_page_screenshot()
             time.sleep(1)
@@ -41,9 +49,24 @@ class WppSender():
     def _get_page_screenshot(self):
         self._driver.get_screenshot_as_file("qr_code.png")
 
-    def receivers_dataframe():
-        pass        
+    def send_messages(self, message:str, contacts:list):
+        message_mod = self._message_format(message)
+        
+        for contact in contacts:
+            url = f"https://web.whatsapp.com/send?phone={contact}&text={message_mod}"
+            self._driver.get(url)
 
+            self._messages_wait()
+            self._click_send_button()
+            self._cycle_sleep_time()
+
+    def _messages_wait(self):
+        while len(self._driver.find_elements(By.XPATH, self.footer_xpath)) < 1:
+            time.sleep(2)
+
+    def _click_send_button(self):
+        self._driver.find_element(By.XPATH, self.send_button_xpath).click()
+     
 
 class WppSenderLayout():
     
--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from RpaWpp import WppSender
 usuario = WppSender(headless=False)
 usuario.screen_headless(True)
 usuario.get_wpp_site()
+
+contatos = ["5531995526168","5531995526168"]
+nomes = ["Guilherme","Gui"]
+
+usuario.send_messages("Testando uma mensagem", contatos)
