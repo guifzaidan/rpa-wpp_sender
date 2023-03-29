5f2d2fa1cdef0e5ff573353b7cdd333db877a19c
Guilherme Zaidan <gfzaidan57@gmail.com>
2023-03-29 20:52:02 -0300

    project finish

--- RpaWpp.py
+++ RpaWpp.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import streamlit as st
 import pandas as pd
 import urllib
 import time
@@ -16,7 +17,7 @@ class WppSender():
 
     @staticmethod
     def _cycle_sleep_time():
-        time.sleep(30)
+        time.sleep(20)
 
     @staticmethod
     def _message_format(message:str):
@@ -26,10 +27,10 @@ class WppSender():
     def __init__(self, headless:bool):
         self._service = Service(ChromeDriverManager().install())
         self._options = webdriver.ChromeOptions()
-        self.screen = self.screen_headless(selection=headless)
+        self.screen = self._screen_headless(selection=headless)
         self._driver = webdriver.Chrome(service=self._service, options=self._options)
 
-    def screen_headless(self, selection:bool):
+    def _screen_headless(self, selection:bool):
         if selection == True:
             self._options.add_argument("--headless")
             # self._options.add_argument("disable-infobars")
@@ -40,10 +41,11 @@ class WppSender():
     def get_wpp_site(self):
         self._driver.get("https://web.whatsapp.com/")
         self._initial_wait()
+        self._driver.minimize_window()
 
     def _initial_wait(self):
         while len(self._driver.find_elements(By.ID, "side")) < 1:
-            self._get_page_screenshot()
+            # self._get_page_screenshot()
             time.sleep(1)
 
     def _get_page_screenshot(self):
@@ -51,14 +53,22 @@ class WppSender():
 
     def send_messages(self, message:str, contacts:list):
         message_mod = self._message_format(message)
-        
-        for contact in contacts:
+        progress_bar = st.progress(0, text="Enviando...")
+
+        for i,contact in enumerate(contacts):
             url = f"https://web.whatsapp.com/send?phone={contact}&text={message_mod}"
             self._driver.get(url)
 
             self._messages_wait()
             self._click_send_button()
-            self._cycle_sleep_time()
+
+            progress_reference = ((i + 1) / len(contacts))
+            progress_bar.progress(progress_reference, text="Enviando...")
+            
+            if progress_reference < 1:
+                self._cycle_sleep_time()
+
+        progress_bar.empty()
 
     def _messages_wait(self):
         while len(self._driver.find_elements(By.XPATH, self.footer_xpath)) < 1:
@@ -66,12 +76,6 @@ class WppSender():
 
     def _click_send_button(self):
         self._driver.find_element(By.XPATH, self.send_button_xpath).click()
-     
-
-class WppSenderLayout():
-    
-    def __init__(self):
-        pass
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 from RpaWpp import WppSender
+import time
+import pandas as pd
+import streamlit as st
 
-usuario = WppSender(headless=False)
-usuario.screen_headless(True)
-usuario.get_wpp_site()
+# --- FRONT-END ----
+st.set_page_config(
+    page_title="Projeto RPA - WhatsApp",
+    page_icon="📫",
+    layout="centered"
+)
 
-contatos = ["5531995526168","5531995526168"]
-nomes = ["Guilherme","Gui"]
+with open("styles.css","r") as css_file:
+    st.markdown(f"<style>{css_file.read()}</style>", unsafe_allow_html=True)
 
-usuario.send_messages("Testando uma mensagem", contatos)
+st.header("📫 Projeto RPA - WhatsApp")
+st.markdown(
+    """
+    O presente projeto consistiu em criar um RPA para envio de mensagens simples de texto via WhatsApp utilizando uma interface gráfica que simula um site.
+    #####
+    """
+)
+
+with st.form("forms"):
+    st.markdown("#### • Preencha os campos abaixo:")
+    message_text = st.text_area("Mensagem que deseja enviar:", max_chars=250, placeholder="Digite sua mensagem automática")
+    st.markdown("#####")
+    uploaded_file = st.file_uploader(
+        label="Faça o upload do seu arquivo de contatos:",
+        type=["xlsx"],
+        accept_multiple_files=False,
+    )
+    st.markdown("#####")
+    st.form_submit_button("Iniciar", type="primary")
+
+# --- BACK-END ----
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    df["CONTATO"] = df["CONTATO"].astype("str")
+    
+    user = WppSender(headless=False)
+    user.get_wpp_site()
+    user.send_messages(message_text, df["CONTATO"])
+
+    success = st.success("Mensagens enviadas com sucesso!", icon="✅")
+    st.balloons()
+    time.sleep(5)
+    success.empty()
